15acfde251ed02649ed6221088312be772770a07
Anna Solovyeva <solovyevaas02@gmail.com>
2020-10-02 16:29:50 +0200

    02.10.2020

--- Virtual Tourist/Controllers/CollectionViewController.swift
+++ Virtual Tourist/Controllers/CollectionViewController.swift
@@ -10,12 +10,26 @@ import CoreData
 
 class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
-    var photos: [UIImage] = []
+    var photos: [NSManagedObject] = []
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         collectionView.reloadData()
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Collection")
+        
+        do {
+            photos = try managedContext.fetch(fetchRequest)
+            print(photos)
+        } catch let error as NSError {
+            print("Could not retrieve data. \(error), \(error.userInfo)")
+        }
     }
     
     override func viewDidLoad() {
@@ -32,7 +46,7 @@ class CollectionViewController: UIViewController, UICollectionViewDelegate, UICo
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCellViewController", for: indexPath) as! CollectionCellViewController
         let photo = photos[indexPath.row]
-        cell.collectionPicture.image = photo
+        cell.collectionPicture.image = UIImage(data: photo.value(forKeyPath: "photo") as! Data)
         return cell
     }
     
@@ -45,11 +59,9 @@ class CollectionViewController: UIViewController, UICollectionViewDelegate, UICo
     }
     
     func imagePickerController(_: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
-        if let image = info[.originalImage] as? UIImage {
-            photos.append(image)
-            print(photos)
+        if let image = info[.originalImage] as? NSData {
+            self.save(photo: image)
         }
-        collectionView.reloadData()
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -62,20 +74,19 @@ class CollectionViewController: UIViewController, UICollectionViewDelegate, UICo
     }
     
     
-    func save(longitude: Double, latitude: Double) {
+    func save(photo: NSData) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
         
         let managedContext = appDelegate.persistentContainer.viewContext
         let entity = NSEntityDescription.entity(forEntityName: "Collection", in: managedContext)!
-        let newLocation = NSManagedObject(entity: entity, insertInto: managedContext)
-        newLocation.setValue(longitude, forKey: "longitude")
-        newLocation.setValue(latitude, forKey: "latitude")
+        let newPhoto = NSManagedObject(entity: entity, insertInto: managedContext)
+        newPhoto.setValue(photo, forKey: "photo")
         
         do {
             try? managedContext.save()
-            locations.append(newLocation)
+            photos.append(newPhoto)
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
         }
